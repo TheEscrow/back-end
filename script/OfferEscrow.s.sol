b51a83a854c8482c66c9f766e9af7d49fb77605d
JHKaller <john.kaller@hotmail.de>
2022-10-28 23:22:04 -0400

    added test and script file

--- script/OfferEscrow.s.sol
+++ script/OfferEscrow.s.sol
@@ -1,1 +1,12 @@
-//Coming soon
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+
+contract OfferEscrowScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+    }
+}
--- test/OfferEscrow.t.sol
+++ test/OfferEscrow.t.sol
@@ -1,1 +1,12 @@
-//Coming soon
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.17;
+
+import "ds-test/test.sol";
+import "src/OfferEscrow.sol";
+
+contract OfferEscrowTest is DSTest {
+
+    //call before running test
+    function setUp() public {
+    }
+}
